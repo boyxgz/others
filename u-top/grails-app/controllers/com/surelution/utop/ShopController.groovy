951bbc4779b1e23a4be246801730c6d20c54b3a4
boyxgz <boyxgz@msn.com>
2015-09-22 09:04:26 +0800

    i

--- u-top/grails-app/controllers/com/surelution/utop/ShopController.groovy
+++ u-top/grails-app/controllers/com/surelution/utop/ShopController.groovy
@@ -428,7 +428,7 @@ class ShopController {
 				if(!giftAmount) {
 					giftAmount = "4"
 				}
-				if(!expiredAt) { 
+				if(!expiredAt) {
 					expiredAt = "20151001000000"
 				}
 				voucher = new Voucher()
