b7f58552ea1e1a66f6021507e763b79b94cecb82
boyxgz <boyxgz@msn.com>
2015-09-22 11:27:58 +0800

    i

--- u-top/grails-app/controllers/com/surelution/utop/OrderFormController.groovy
+++ u-top/grails-app/controllers/com/surelution/utop/OrderFormController.groovy
@@ -37,4 +37,37 @@ class OrderFormController {
 		}
 		redirect(action:'index')
 	}
+
+	/**
+	 * 加油站只可以激活受限时间的锁定码
+	 * @return
+	 */
+	def limitedList() {
+		def now = new Date()
+		def from
+		def to
+		use(TimeCategory) {
+			from = now - 60.minutes
+			to = now - 2.minutes
+		}
+		def vouchers = Voucher.executeQuery("""
+			from Voucher v where v.order.dateCreated between ? and ? and v.order.status = ? and v.status = ? order by v.id
+		""", [from, to, SaleOrderStatus.QUERYED_TO_PAY, VoucherStatus.QUERIED_TO_USE])
+		[vouchers:vouchers]
+	}
+
+	def limitedActive() {
+		def voucherId = params.voucherId
+		if(voucherId) {
+			Voucher voucher = Voucher.get(voucherId)
+			if(voucher?.status == VoucherStatus.QUERIED_TO_USE) {
+				voucher.status = VoucherStatus.NEW
+				voucher.order = null
+				voucher.writeOff = null
+				voucher.save(flush:true)
+			}
+		}
+		redirect(action:'limitedList')
+	}
+	
 }
