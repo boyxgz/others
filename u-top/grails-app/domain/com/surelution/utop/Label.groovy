6c8a88c3ee624a5899c8c5cb9df9fe5df9f21914
boyxgz <boyxgz@msn.com>
2015-11-09 16:06:19 +0800

    i

--- u-top/grails-app/domain/com/surelution/utop/Label.groovy
+++ u-top/grails-app/domain/com/surelution/utop/Label.groovy
@@ -1,9 +1,20 @@
 package com.surelution.utop
 
+/**
+ * 
+ * @author <a href="mailto:guangzong.syu@gmail.com">guagnzong</a>
+ *
+ */
 class Label {
 
     static constraints = {
+		key unique:true
     }
 	
+	static mapping = {
+		key column:'_key'
+	}
+	
 	String name
+	String key
 }
